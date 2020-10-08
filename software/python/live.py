741d104b2f12114e954481c6fc67cd052cafb836
Adrian Studer <github@adrianstuder.com>
2020-10-08 13:58:00 -0700

    add option to automatically scale image data

--- software/python/live.py
+++ software/python/live.py
@@ -11,9 +11,9 @@ camera = Camera()
 def loop(i):
     camera.capture() 
     yar = camera.getPixels()
-    # print(yar)
     ax1.clear()
-    ax1.set_ylim((0, 3000), auto=False)
+    if args.auto_scale == False:
+        ax1.set_ylim((0, 3000), auto=False)
     ax1.plot(yar)
 
 # read command line arguments
@@ -21,6 +21,7 @@ def loop(i):
 parser = argparse.ArgumentParser(description="EPC901 Live View 0.1")
 parser.add_argument("-p", help="serial port of camera, for example /dev/serial0 or COM3", dest="port", required=True)
 parser.add_argument("-e", help="exposure time in milliseconds", dest="exposure", default=1000)
+parser.add_argument("-a", help="automatically scale based on image content", dest="auto_scale", action="store_true", default=False)
 args = parser.parse_args()
 
 camera.open(args.port)
