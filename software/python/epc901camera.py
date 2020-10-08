167cd5746a2945e03a290491de1a7536d5a32930
Adrian Studer <github@adrianstuder.com>
2020-10-08 10:18:13 -0700

    split epc901 camera class into its own library

--- software/python/epc901camera.py
+++ software/python/epc901camera.py
@@ -1,11 +1,8 @@
-# Test application for EPC901 breakout board
+# Wrapper library for EPC901 breakout board
 
 import serial
 import sys
-import argparse
 from textwrap import wrap
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 class Camera:    
     ser = serial.Serial()
@@ -40,29 +37,3 @@ class Camera:
         frame_data = [int(x, 16) for x in wrap(self.ser.readline().decode("utf-8"), 4)]
         return frame_data
 
-camera = Camera()
-
-def loop(i):
-    camera.capture() 
-    yar = camera.getPixels()
-    # print(yar)
-    ax1.clear()
-    ax1.set_ylim((0, 3000), auto=False)
-    ax1.plot(yar)
-
-# read command line arguments
-# we expect camera.py -p <serial port> -e <exposure time in ms>
-parser = argparse.ArgumentParser(description="EPC901 camera 0.1")
-parser.add_argument("-p", help="serial port of camera, for example /dev/serial0 or COM3", dest="port", required=True)
-parser.add_argument("-e", help="exposure time in milliseconds", dest="exposure", default=1000)
-args = parser.parse_args()
-
-camera.open(args.port)
-camera.setExposure(args.exposure)
-
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-
-ani = animation.FuncAnimation(fig, loop, interval=100)
-plt.show()
-camera.close()
---
+++ software/python/live.py
@@ -0,0 +1,34 @@
+# Live view for EPC901 breakout board
+
+from epc901camera import Camera
+import sys
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+camera = Camera()
+
+def loop(i):
+    camera.capture() 
+    yar = camera.getPixels()
+    # print(yar)
+    ax1.clear()
+    ax1.set_ylim((0, 3000), auto=False)
+    ax1.plot(yar)
+
+# read command line arguments
+# we expect camera.py -p <serial port> -e <exposure time in ms>
+parser = argparse.ArgumentParser(description="EPC901 Live View 0.1")
+parser.add_argument("-p", help="serial port of camera, for example /dev/serial0 or COM3", dest="port", required=True)
+parser.add_argument("-e", help="exposure time in milliseconds", dest="exposure", default=1000)
+args = parser.parse_args()
+
+camera.open(args.port)
+camera.setExposure(args.exposure)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+ani = animation.FuncAnimation(fig, loop, interval=100)
+plt.show()
+camera.close()
